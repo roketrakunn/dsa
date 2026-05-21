5833b876b986a431713ea958285163e2d86921ca
Roket <okuhlexesibe0@gmail.com>
2026-05-21 19:40:42 +0200

    bubble sort done

---
+++ src/sorting/mod.rs
@@ -0,0 +1,19 @@
+pub fn bubble_sort(arr:  &mut Vec<i32>) {
+    
+    for _ in 0..arr.len() { 
+
+        let mut swapped = false;
+
+        for i in 0..arr.len()-1 { 
+            if arr[i]  > arr[i+1] { 
+                arr.swap(i, i+1);
+                swapped = true
+            }
+        }
+
+        if swapped == false { 
+            break;
+        }
+    }
+}
+
